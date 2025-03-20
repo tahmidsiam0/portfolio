3a6ecec1b08a63d4d245526d373489cf37271066
Siam <tahmidsiam0@gmail.com>
2025-03-20 21:02:49 +0600

    gsap applied on contact

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,20 +1,41 @@
+import { useGSAP } from "@gsap/react/src";
 import FancyDivider from "../Components/Common/FancyDivider";
 import ContactDetails from "../Components/Contact/ContactDetails";
 import ContactForm from "../Components/Contact/ContactForm";
-
+import gsap from "gsap";
 
 export default function ContactPage() {
-
-    // useGSAP
+  useGSAP(() => {
+    gsap.fromTo(
+      ".contactPage",
+      { y: 50 },
+      { y: 0, opacity: 1, duration: 0.5, ease: "back", delay: 0.5 }
+    );
+  }, []);
+  // useGSAP(() => {
+  //   gsap.fromTo(
+  //     ".contactPage",
+  //     {
+  //       y: 100,
+  //       opacity: 0,
+  //     },
+  //     { y: 0, duration: 2, opacity: 1 }
+  //   );
+  //   // gsap.from(".contactPage", { y: 100 });
+  // }, []);
 
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-4xl">Let's Work Together</h1>
-      <FancyDivider className="w-75" />
-      <div className="grid grid-cols-2 pt-2">
-        <ContactDetails />
-        <ContactForm />
+    <>
+      <div className="flex flex-col items-center contactPage opacity-0">
+        <div>
+          <h1 className="text-4xl ">Let's Work Together</h1>
+        </div>
+        <FancyDivider className="w-75" />
+        <div className="grid grid-cols-2 pt-2">
+          <ContactDetails />
+          <ContactForm />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
